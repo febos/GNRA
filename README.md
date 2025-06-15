991ac4f81354a530e18d187da4660a7b45474403
Eugene Baulin <efbaulin@gmail.com>
2025-06-15 23:18:01 +0200

    FigurePenta & README

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # GNRA
-Exploring the GNRA-tetraloop-like folds in nucleic acid 3D structures
+Exploring GNRA tetraloop-like motifs in nucleic acid 3D structures
+
+## Reference
+
+[TODO](#)
+
+## Check out [our other developments](https://github.com/febos/wiki)
+
+## Content 
+
+### Figures
+
+Manuscript figures
+
+### Variants
+
+The representative matches of the twelve recurrent backbone topology GNRA-tetraloop motif variants in PDB format
+
+### files
+
+- **8VTW_CGAAAG.cif** - The six-residue reference in mmCIF format
+- **8VTW_GAAA.cif** - The four-residue reference in mmCIF format
+- **CGAAAG.artem** - Raw ARTEM output for the six-residue reference search
+- **FigureRMSDthresh.py** - Python script to plot RMSD distributions
+- **GAAA.artem** - Raw ARTEM output for the four-residue reference search
+- **GNRA_pairwise_RMSD.csv** - Pairwise RMSD values among six-residue motifs
+- **GNRA_pairwise_RMSD4.csv** - Pairwise RMSD values among four-residue motifs
+- **HL_85603.2.csv** - The GNRA motif class of the [RNA 3D Motif Atlas](https://rna.bgsu.edu/rna3dhub/motifs)
+- **README.md** - This README file
+- **REPRODUCE.md** - The steps to reproduce the results
+- **TableS1.xlsx** - Table S1. List of 23,283 non-redundant matches of the GNRA motif.
+- **TableS2.xlsx** - Table S2. List of 13,729 unique instances of the GNRA tetraloop motif matches
+- **TableS3.xlsx** - Table S3. List of 59 unique instances of the GNRA tetraloop motif strand topologies
+- **annotate_hits.py** - Python script to annotate ARTEM matches with structural features
+- **annotated_hits.tsv** - The list of annotated matches
+- **choose_4rmsdmax.py** - Python script to select the RMSD thresholds
+- **choose_GNRA_reference.py** - Python script to select the GNRA tetraloop motif reference
+- **filter_hits.py** - Python script to filter redundant matches
+- **nr_hits.tsv** - The list of non-redundant matches
+- **nrlist_3.370_all.csv** - [The BGSU representative set of RNA structures](https://rna.bgsu.edu/rna3dhub/nrlist)
+- **pdb_download.py** - Python script to download nucleic acid-containing PDB entries
+- **pdb_process.py** - Python script to process the PDB entries
+- **postprocess_artem.py** - Python script to parse ARTEM matches
+- **processed_hits.tsv** - The list of parsed ARTEM matches
+- **run_DSSR.py** - Python script to run DSSR annotations
+- **stat.ipynb** - Jupyter Notebook with summary statistics of the dataset
+- **unique_hits.tsv** - The list of structurally unique matches
+- **unique_topologies.tsv** - The list of unique backbone topology variants of the GNRA tetraloop motif
--- filter_hits.py
+++ filter_hits.py
@@ -68,7 +68,7 @@ with open("nr_hits.tsv") as inp:
 
 
             link = "https://rna.bgsu.edu/rna3dhub/display3D/unitid/{}"\
-                   .format(','.join([pdb.upper()+'|'+x.replace('.','|')[:-1] for x in rs if x != '-']))
+                   .format(','.join([pdb[-4:].upper()+'|'+x.replace('.','|')[:-1] for x in rs if x != '-']))
             
             desc = ':'.join([str(x) for x in [size, matchmask, *bs, crystalmask,
                                               *conns, *rels, *elems, loopseq,
@@ -130,7 +130,7 @@ with open("nr_hits.tsv") as inp:
 
 
             link = "https://rna.bgsu.edu/rna3dhub/display3D/unitid/{}"\
-                   .format(','.join([pdb.upper()+'|'+x.replace('.','|')[:-1] for x in rs if x != '-']))
+                   .format(','.join([pdb[-4:].upper()+'|'+x.replace('.','|')[:-1] for x in rs if x != '-']))
             
             desc = topo
 
@@ -144,6 +144,50 @@ with open("nr_hits.tsv") as inp:
             
 
 with open("unique_topologies.tsv","w") as outp:
+    for cnt,line, desc, topo,link,pdb in sorted([(v[1],v[2],k,v[3],v[4],v[5]) for k,v in bag.items()]):
+        outp.write(line.strip()+'\t'+str(len(counts[desc][0]))+'\t'+str(len(counts[desc][1]))+'\t'+\
+                   topo+'\t'+\
+                   ','.join(sorted(counts[desc][0]))+'\t'+','.join(sorted(counts[desc][1]))+'\t'+link+'\n')
+
+
+
+bag = {}
+counts = {}
+
+with open("nr_hits.tsv") as inp:
+    cnt = 0
+    for line in inp:
+        cnt += 1
+        if line.strip():
+            hit = line.strip().split()
+            
+            pdb, size, rmsd, rmsdsize, resrmsd, matchmask,\
+            rs, bs, crystalmask, conns, rels, elems,\
+            loopseq, loopsize, confs, gabp, flbp, inters,\
+            ut, rfam, nrclass = *hit[:6],hit[6:12],hit[12:18],hit[18],hit[19:24],hit[24:29],\
+                                hit[29:35],hit[35],hit[36],hit[37:43],hit[43],hit[44],hit[45:51],\
+                                hit[51],hit[52],hit[53]
+            rmsd = float(rmsd)
+            topo = loopsize+'-'+'-'.join(conns)
+
+            if loopsize not in ('5','6'):
+                continue
+
+            link = "https://rna.bgsu.edu/rna3dhub/display3D/unitid/{}"\
+                   .format(','.join([pdb[-4:].upper()+'|'+x.replace('.','|')[:-1] for x in rs if x != '-']))
+            
+            desc = topo
+
+            if desc not in counts:
+                counts[desc] = [set(), set()]
+            counts[desc][0].add(rfam)
+            counts[desc][1].add(nrclass)
+
+            if desc not in bag or bag[desc][0] > rmsd:
+                bag[desc] = [rmsd,cnt,line,topo,link,pdb]   
+            
+
+with open("penta_hexa.tsv","w") as outp:
     for cnt,line, desc, topo,link,pdb in sorted([(v[1],v[2],k,v[3],v[4],v[5]) for k,v in bag.items()]):
         outp.write(line.strip()+'\t'+str(len(counts[desc][0]))+'\t'+str(len(counts[desc][1]))+'\t'+\
                    topo+'\t'+\
